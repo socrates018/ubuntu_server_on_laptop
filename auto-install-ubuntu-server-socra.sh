5875b1c5a596ef2dbd6a4fb3f53a5ccb03333b49
socrates018 <socratessuciu8@gmail.com>
2025-10-14 22:41:39 +0300

    add script
    it runs ubuntu-autoinstall-generator.sh

---
+++ auto-install-ubuntu-server-socra.sh
@@ -0,0 +1,8 @@
+sudo SKIP_VERIFY=true ./ubuntu-autoinstall-generator.sh \
+  -k \
+  -a \
+  -u ~/autoinstall-config/user-data \
+  -m ~/autoinstall-config/meta-data \
+  -s /mnt/d/socra/Downloads/ubuntu-24.04.3-live-server-amd64.iso \
+  -d /mnt/d/socra/Downloads/ubuntu-auto.iso
+  
