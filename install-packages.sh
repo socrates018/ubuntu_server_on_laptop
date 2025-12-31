2c155e04c48fb31047f454a56ba4ba9d0f148387
socrates018 <socratessuciu8@gmail.com>
2026-01-01 01:21:01 +0200

    add install-packages script

---
+++ install-packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+[ "$EUID" -ne 0 ] && echo "Run with sudo" && exit 1
+
+apt-get update -qq
+apt-get install -y -qq ddclient cockpit curl git htop net-tools ufw wireguard neofetch nano unattended-upgrades acpi tlp miniupnpc cron
+
+systemctl restart cockpit.socket 2>/dev/null
